ae4a596fe02e930dcf76dac8116abcae458ff4d7
puneet-ahuja <ahujapuneet92@gmail.com>
2017-08-10 17:53:35 +0530

    Add files via upload

--- REDME.md
+++ REDME.md
@@ -1,3 +1,7 @@
 npm init
 
 npm install body-parser express --save
+
+
+to start express server
+node index.js
--- index.js
+++ index.js
@@ -4,10 +4,18 @@ var fs = require('fs');
 
 var app = express();
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+}); 
+
+
 app.use(bp.urlencoded({
   extended: true
 }));
 app.use(bp.json());
+
 
 
 
@@ -18,18 +26,46 @@ app.get('/test', function (req, res) {
 
 
 
-	// initialisation of variables
 
-	// decalre functions
 	function handleHomepageGet(req, res){
 		fs.readFile('data/homepage.json', function (err,data) {
 			res.send(JSON.parse(data));
 		});
 	}
+	function handleSeason1Get(req, res){
+		fs.readFile('data/season1.json', function (err,data) {
+			res.send(JSON.parse(data));
+		});
+	}
+		function handleSeason2Get(req, res){
+		fs.readFile('data/season2.json', function (err,data) {
+			res.send(JSON.parse(data));
+		});
+	}
+		function handleSeason3Get(req, res){
+		fs.readFile('data/season3.json', function (err,data) {
+			res.send(JSON.parse(data));
+		});
+	}
+		function handleSeason4Get(req, res){
+		fs.readFile('data/season4.json', function (err,data) {
+			res.send(JSON.parse(data));
+		});
+	}
+		function handleSeason5Get(req, res){
+		fs.readFile('data/season5.json', function (err,data) {
+			res.send(JSON.parse(data));
+		});
+	}
 
 app.get('/homepage'  , handleHomepageGet)
+app.get('/season1'  , handleSeason1Get)
+app.get('/season2'  , handleSeason2Get)
+app.get('/season3'  , handleSeason3Get)
+app.get('/season4'  , handleSeason4Get)
+app.get('/season5'  , handleSeason5Get)
 
-// start application
+// Start Express server
 app.listen(3001, function () {
   
   console.log('Example app listening on port 3000!');
